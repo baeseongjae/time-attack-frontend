10805f34c04cd34039172d6de5c502cb0370e457
baeseongjae <3574214@naver.com>
2024-02-13 09:30:34 +0900

    ✨ feat: 페이지 컴포넌트 생성 및 기본 루트 레이아웃 구성

---
+++ src/app/(root)/layout.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function RootLayout({ children }: { children: React.ReactNode }) {
+  return <>{children}</>;
+}
+
+export default RootLayout;
---
+++ src/components/Page/Page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Page({ children }: { children: React.ReactNode }) {
+  return <main>{children}</main>;
+}
+
+export default Page;
